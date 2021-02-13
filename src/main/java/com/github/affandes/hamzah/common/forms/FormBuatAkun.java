1ca0276c1181dc0036a7140863a6173fb83c5709
Affandes <affandes@uin-suska.ac.id>
2021-02-13 08:42:48 +0700

    Initial commit

---
+++ src/main/java/com/github/affandes/hamzah/common/forms/FormBuatAkun.java
@@ -0,0 +1,21 @@
+package com.github.affandes.hamzah.common.forms;
+
+/**
+ * Form untuk menambahkan akun baru
+ */
+public class FormBuatAkun {
+    /**
+     * Email
+     */
+    public String email;
+
+    /**
+     * Sandi
+     */
+    public String sandi;
+
+    /**
+     * Nama
+     */
+    public String nama;
+}
---
+++ src/main/java/com/github/affandes/hamzah/common/models/Akun.java
@@ -0,0 +1,23 @@
+package com.github.affandes.hamzah.common.models;
+
+/**
+ * Model Akun
+ * Digunakan untuk standarisasi format
+ * class Akun
+ */
+public class Akun {
+    /**
+     * id yang digenerate dari db
+     */
+    public String id;
+
+    /**
+     * alamat email
+     */
+    public String email;
+
+    /**
+     * nama akun
+     */
+    public String nama;
+}
